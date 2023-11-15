fec790c46cc51c4222ddc5c680d5fe13db15261a
Mateo Ortiz <jeusmateo@gmail.com>
2023-11-15 11:58:13 -0600

    Add VS Code settings and ignore files to gitignore

---
+++ src/PalabrasReservadas.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+# PROGRAMA
+# FINPROG
+# SI
+# ENTONCES
+# SINO
+# FINSI
+# REPITE
+# VECES
+# FINREP
+# IMPRIME
+# LEE
+class PalabrasReservadas(Enum):
+    PROGRAMA = 1
+    FINPROG = 2
+    SI = 3
+    ENTONCES = 4
+    SINO = 5
+    FINSI = 6
+    REPITE = 7
+    VECES = 8
+    FINREP = 9
+    IMPRIME = 10
+    LEE = 11
