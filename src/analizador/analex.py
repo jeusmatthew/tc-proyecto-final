3d4febcdf2578c3f6dd6e6a717e67cdb78903eac
RaulMagaña <maganaraul349@hotmail.com>
2023-12-11 11:33:41 -0600

    Refactor code for analyzing and cleaning lines

--- src/analizador/analex.py
+++ src/analizador/analex.py
@@ -1,12 +1,66 @@
 import sys
 import os
 import re
-import AnalizadorLexico as al
 
 # abre el archivo por medio de argumentos
-def open_file():
+
+
+def abrir_archivo():
     with open("src/factorial.mio", "r", encoding="UTF-8") as file:
         return file.read(), file.name
+
+
+def analizar(codigo: str):
+    """
+    1. separa el codigo por lineas
+    2. limpia todas las lineas 
+    3. verifica la linea para saber que instruccion es
+        1. si es un comentario iniciará con '#'
+        2. si es un
+
+    """
+    lines = separar_lineas(codigo)
+    lines = limpiar_lineas(lines)
+
+    for index, line in enumerate(lines):
+        if es_comentario(line):
+            lines.pop(index)
+
+        elif es_palabra_reservada(line):
+            print(f"{line} es una palabra reservada")
+
+
+def separar_lineas(raw_data: str):
+    return raw_data.split("\n")
+
+
+def limpiar_lineas(lines: list[str]):
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
+    return lines
+
+
+def es_comentario(line: str):
+    return line[0] == "#"
+
+
+def es_palabra_reservada(line: str):
+    # PROGRAMA
+    # FINPROG
+    # SI
+    # ENTONCES
+    # SINO
+    # FINSI
+    # REPITE
+    # VECES
+    # FINREP
+    # IMPRIME
+    # LEE
+    reserved_words = ["PROGRAMA", "FINPROG", "SI", "ENTONCES",
+                      "SINO", "FINSI", "REPITE", "VECES", "FINREP", "IMPRIME", "LEE"]
+
+    return line in reserved_words
+
 
 def main():
     # variables
@@ -14,11 +68,11 @@ def main():
     word_list = []
 
     # abre el archivo
-    read_data, filename = open_file()
+    read_data, filename = abrir_archivo()
     filename = os.path.splitext(filename)[0]
     fileOut = open(f"{filename}.lex", "w")
 
-    al.analizar(read_data)
+    analizar(read_data)
 
 
 if __name__ == "__main__":
